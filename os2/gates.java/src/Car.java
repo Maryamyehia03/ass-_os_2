dd4699ec6bb94c1cd35daf3fabf58ef653eb5cc0
Mayam Yehia <114694952+Maryamyehia03@users.noreply.github.com>
2024-11-05 20:18:25 +0200

    Update Car.java

--- os2/gates.java/src/Car.java
+++ os2/gates.java/src/Car.java
@@ -2,11 +2,13 @@ public class Car extends Thread {
     private final int carId;
     private final int arrivalTime; 
     private final int parkeId;
+    private final int gateId;
 
-    public Car(int carId, int arrivalTime,int parkeId ) {
+    public Car(int carId, int arrivalTime,int parkeId , int gateId ) {
         this.carId = carId;
         this.parkeId = parkeId; 
         this.arrivalTime = arrivalTime;
+        this.gateId=gateId;
     }
     public void arrive(){
         System.out.println(carId + " " + arrivalTime + " " + parkeId );
@@ -15,5 +17,14 @@ public class Car extends Thread {
   
     public int getCarId() {
         return carId;
+    }
+    public int getGateId() {
+        return gateId;
+    }
+    public int getParkeId() {
+        return parkeId;
+    }
+    public int getArrivalTime() {
+        return arrivalTime;
     }
 }
